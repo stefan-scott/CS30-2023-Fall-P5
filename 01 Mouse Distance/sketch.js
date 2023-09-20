1d5e2f0574ae5b6e4999ae58f2c6a27fc2098936
stefan-scott <scottst@spsd.sk.ca>
2023-09-20 15:18:47 -0600

    finished demo

--- 01 Mouse Distance/sketch.js
+++ 01 Mouse Distance/sketch.js
@@ -1,15 +1,58 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Mouse Distance
+// Mr. Scott
+// Sept 20, 2023
+// arrays, maths, functions, scroll wheel, colors
 
+// Global Variables
+let colorArray = [];   //red, green, blue
+let colorIndex = 0;    //  0    1     2          
+
+function createColors(){  //simplest:  no inputs, no return
+  colorArray.push(color("red"));
+  colorArray.push(color(200,100,255));
+  colorArray.push(color("midnightblue"));
+}
+
+function mouseWheel(event){
+  //up → negative   down → pos
+  print(event.delta);
+  if(event.delta < 0){  //UP
+    colorIndex += 1;
+    if(colorIndex > colorArray.length-1){
+      colorIndex = 0;
+    }
+  }
+}
+
+function mouseDistance(x1,y1,x2,y2){
+  //given two ordered pairs, return the 
+  //straightline distance between
+  let a = Math.abs(x1-x2);
+  let b = Math.abs(y1-y2);
+  let c = Math.sqrt(a*a + b*b);
+  return c;
+}
+
+function drawNodes(){
+  // draw two "nodes" (circles) at center and mouse
+  // and connect with a single edge
+  fill(colorArray[colorIndex]);
+  stroke(colorArray[colorIndex]);
+  circle(width/2,height/2, 20);
+  circle(mouseX,mouseY, 20);
+  line(width/2,height/2,mouseX,mouseY);
+  let d = mouseDistance(mouseX,mouseY,width/2,height/2);
+  textSize(30);
+  textAlign(CENTER);
+  text(round(d,1), width/2, height * 0.6);
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createColors();
 }
 
 function draw() {
   background(220);
+  drawNodes();
 }
