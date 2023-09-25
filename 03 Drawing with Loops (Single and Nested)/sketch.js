7c6f1607024251cc5d6d16aff54c0ba076569546
stefan-scott <scottst@spsd.sk.ca>
2023-09-25 14:49:46 -0600

    Update sketch.js

--- 03 Drawing with Loops (Single and Nested)/sketch.js
+++ 03 Drawing with Loops (Single and Nested)/sketch.js
@@ -3,10 +3,24 @@
 // Sept 25, 2023
 // Generating a single image with loops
 
+// Global Variables
+let numSegments = 10; 
+let segmentHeight;  //height/numSegments.
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  segmentHeight = height/numSegments;
+}
+
+function gradient(){
+  // use a loop to create a gradient background
+  for(let i = 0; i < numSegments; i++){
+    let y = i * segmentHeight;
+    fill(y);
+    rect(0,y,width, segmentHeight);
+  }
 }
 
 function draw() {
-  background(220);
+  gradient();
 }
