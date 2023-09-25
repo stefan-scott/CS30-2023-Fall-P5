ebb19871d5343260f2d4f9804884885f3c787417
stefan-scott <scottst@spsd.sk.ca>
2023-09-25 14:24:19 -0600

    Add to project

---
+++ 03 Drawing with Loops (Single and Nested)/sketch.js
@@ -0,0 +1,12 @@
+// Drawing with Single and Nested Loops
+// Mr. Scott
+// Sept 25, 2023
+// Generating a single image with loops
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
