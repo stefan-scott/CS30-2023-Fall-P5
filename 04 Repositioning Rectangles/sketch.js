19c0f5bd10b59b085fe50f05ba06577ac20c0105
stefan-scott <scottst@spsd.sk.ca>
2023-09-27 15:11:27 -0600

    Update sketch.js

--- 04 Repositioning Rectangles/sketch.js
+++ 04 Repositioning Rectangles/sketch.js
@@ -7,16 +7,57 @@
 let x, y, rWidth, rHeight;
 let rLeft, rRight, rTop, rBottom;
 let mouseOver = false;  //are we hovering over the rect?
+let pickedUp = false; //are we currently moving it?
+let xOff, yOff;  //so we can grab away from center
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
-  x = width/2,  y = height/2;
-  rWidth = 200;   rHeight = 100;
+  x = width / 2, y = height / 2;
+  rWidth = 200; rHeight = 100;
 }
 
-function drawRectangle(){
+function updateEdgePositions() {
+  //update the left/right/top/bottom properties
+  rLeft = x - rWidth / 2; rRight = x + rWidth / 2;
+  rTop = y - rHeight / 2; rBottom = y + rHeight / 2;
+}
+
+function drawRectangle() {
   //render the rectangle and handle dragging...
+  updateEdgePositions();
+  print(rLeft, rRight, rTop, rBottom);
+
+  //check if mouse is OVER the rect or not?
+  if (mouseX > rLeft && mouseX < rRight &&
+    mouseY > rTop && mouseY < rBottom) {
+    mouseOver = true;
+    fill(170, 190, 50);
+  }
+  else {
+    mouseOver = false;
+    fill(35, 70, 125);
+  }
+
+  if(pickedUp){
+    x = mouseX + xOff;
+    y = mouseY + yOff;
+  }
+
+  rect(x, y, rWidth, rHeight);
+}
+
+function mousePressed(){
+  //triggers exactly 1 per click (on down action)
+  if(mouseOver){
+    pickedUp = true;
+    xOff = x - mouseX;
+    yOff = y - mouseY;
+  }
+}
+
+function mouseReleased(){
+  pickedUp = false;
 }
 
 function draw() {
