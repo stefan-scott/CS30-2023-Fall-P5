778535c8f9e20e608d6f450edfd0e7e4e99746e8
stefan-scott <scottst@spsd.sk.ca>
2023-10-04 15:01:57 -0600

    before noise

--- 06 Noisy Numbers/sketch.js
+++ 06 Noisy Numbers/sketch.js
@@ -10,14 +10,19 @@ let ballY = 200;  let ySpeed;
            
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(15);  //temporary...to make viewing easier
 }
 
 function segmentLine(){
   //using a loop, draw one line made up of many
-  strokeWeight(5);
+  strokeWeight(15);
   
   let x = 0; //loop variable
   while (x < width){
+    //option 1: random()
+    let greyValue = random(0,255);
+
+    stroke(greyValue);
     line(x, height/2, x+segL, height/2);
     x += segL;
   }
@@ -25,16 +30,21 @@ function segmentLine(){
 
 function rectangleOnLine(){
   //draw a single *rounded* rectangle on the line
+  //option 1: random()
+  let radius = random(2,70);
+  
   rectMode(CORNERS);
   strokeWeight(2);
   stroke(0);
 
-  rect(width*0.1,height/2,width*0.5, height*0.25);
+  rect(width*0.1,height/2,width*0.5, height*0.25,radius);
 }
 
 function moveBall(){
+  //option 1:  random()
+  ySpeed = random(-20,20); 
 
-
+  ballY += ySpeed;
   circle(width*0.7, ballY, 30);
 }
 
