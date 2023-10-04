b9474025e9bec1c1fc2c2b3447b05a35d6ed3700
stefan-scott <scottst@spsd.sk.ca>
2023-10-04 14:42:19 -0600

    rect done

--- 06 Noisy Numbers/sketch.js
+++ 06 Noisy Numbers/sketch.js
@@ -1,15 +1,38 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Noisy Numbers
+// Mr. Scott
+// October 4, 2023
+// Some simple animations using
+// random()  and   noise()
 
-
+// Global Variables
+let segL = 10;
+           
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function segmentLine(){
+  //using a loop, draw one line made up of many
+  strokeWeight(5);
+  
+  let x = 0; //loop variable
+  while (x < width){
+    line(x, height/2, x+segL, height/2);
+    x += segL;
+  }
+}
+
+function rectangleOnLine(){
+  //draw a single *rounded* rectangle on the line
+  rectMode(CORNERS);
+  strokeWeight(2);
+  stroke(0);
+
+  rect(width*0.1,height/2,width*0.5, height*0.25);
+}
+
 function draw() {
   background(220);
+  segmentLine();
+  rectangleOnLine();
 }
