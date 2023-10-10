c07a21886d0fb1c15c00cd725c12021689931934
stefan-scott <scottst@spsd.sk.ca>
2023-10-10 15:17:12 -0600

    finished copy of demo

--- 08 Working with Images/sketch.js
+++ 08 Working with Images/sketch.js
@@ -7,29 +7,40 @@
 let lionL, lionR; //store Image object in each
 let facingLeft = true;
 let pinImages = []; //to hold all our pinwheel images
+let pinIndex = 0;
 
 function preload(){
   //happens before setup(). Waits for loading to finish
   lionL = loadImage("assets/lion-left.png");
   lionR = loadImage("assets/lion-right.png");
   for(let i=0; i<9; i++){
-   pinImages.push(loadImage("assets/pin-0"+i+".png"));
+    pinImages.push(loadImage("assets/pin-0"+i+".png"));
   }
   
-  
-  
-
-
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
+  //frameRate(3);  //GLOBAL ATTRIBUTE
 }
 
 function draw() {
   background(220);
-  //drawLion();
+  drawLion();
+  drawPin();
+}
+
+function drawPin(){
+  //animate our pinwheel images
+  
+  image(pinImages[pinIndex],width/2,height/2);
+  
+  if(frameCount % 4 === 0){  //only on even frames
+    pinIndex ++;
+      if(pinIndex > 8) pinIndex = 0;
+  }
+  
 }
 
 function drawLion(){
