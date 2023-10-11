0c1186a90f65d1717202e619c05ca85d380ca089
stefan-scott <scottst@spsd.sk.ca>
2023-10-11 15:06:49 -0600

    demo done

--- 09 Snake/sketch.js
+++ 09 Snake/sketch.js
@@ -6,21 +6,55 @@
 //Global Variables
 let points = []; //snake coordinates
 let speed = 6;  //snake speed
-let snakeLength = 10;
+let snakeLength = 30;
 let snakeLocation;  //a Point for the where the head is
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  snakeLocation = new Point(width/2, height/2);
+  snakeLocation = new Point(width / 2, height / 2);
   strokeWeight(15);
+  initSnake(); //populate the array
+}
+
+function initSnake() {
+  //place in points[] a bunch of starting Points
+  for (let i = 0; i < snakeLength; i++) {
+    points.push(createPoint());
+  }
+}
+
+function createPoint() {
+  //return a new Point object ↑ ↓ → ←
+  if (keyCode === UP_ARROW) snakeLocation.y -= speed;
+  else if (keyCode === DOWN_ARROW) snakeLocation.y += speed;
+  else if (keyCode === LEFT_ARROW) snakeLocation.x -= speed;
+  else if (keyCode === RIGHT_ARROW) snakeLocation.x += speed;
+  //if a different coded key was pressed, no change
+  return new Point(snakeLocation.x, snakeLocation.y);
 }
 
 function draw() {
   background(220);
+  moveAndDisplay();
 }
 
-class Point{ //a class for an (x,y) point
-  constructor(x,y){
+function moveAndDisplay(){
+  // loop through the array, and connect all points
+  for(let i = 0; i < points.length-1; i++){
+    let cur = points[i];  //Point  .x  .y
+    let next = points[i+1];
+    let alpha = map(i,0,points.length-1,0,150);
+    stroke(0,alpha); 
+    line(cur.x, cur.y, next.x, next.y);
+  }
+
+  //move -delete first element, push new point
+  points.splice(0,1);   //deletes element 0
+  points.push(createPoint());
+}
+
+class Point { //a class for an (x,y) point
+  constructor(x, y) {
     this.x = x;
     this.y = y;
   }
