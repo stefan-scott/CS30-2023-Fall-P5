4b1955604ce0de5b01ec627e7a201d9f11fe94a2
stefan-scott <scottst@spsd.sk.ca>
2023-11-07 14:57:16 -0600

    completed demo

--- 17 - Puzzle Game Starter/sketch.js
+++ 17 - Puzzle Game Starter/sketch.js
@@ -24,13 +24,33 @@ function draw() {
   print(col,row);
 }
 
+function mousePressed(){
+  // when the mouse is pressed, flip the corresponding 2d array value
+  // also going to flip 4 cardinal neighbours (N, S, E, W)
+  flip(col,row);  //flip "active tile"
+
+  // flip the neighbouring tiles too
+  if(row>0) flip(col, row-1);  //flip tile above
+  if(col < NUM_COLS-1) flip(col+1, row); //flip right tile
+
+}
+
+function flip(x,y){
+  //at a given x,y. flip the value in our 2D array
+  //0 → 255      255 → 0
+  if (grid[y][x] === 0) grid[y][x] = 255;
+  else grid[y][x] = 0;
+}
+
+
+
 function getCurrentX(){ //return the current column mouse is in
-  let constrainX = constrain(mouseX, 0 , width);
+  let constrainX = constrain(mouseX, 0 , width-1);
   return int(constrainX/rectWidth);
 }
 
 function getCurrentY(){ //return the current row mouse is in
-  let constrainY = constrain(mouseY, 0, height);
+  let constrainY = constrain(mouseY, 0, height-1);
   return int(constrainY/rectHeight);
 }
 
